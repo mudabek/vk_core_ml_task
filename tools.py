6242713751fbec865be08da1a9d364b41c711fef
mudabek <nazarovo@grinnell.edu>
2022-04-04 10:51:13 +0400

    fixes

--- tools.py
+++ tools.py
@@ -1,5 +1,113 @@
 import pandas as pd
 import numpy as np
+from scipy import sparse
+import itertools
+from lightfm import LightFM
+from lightfm.evaluation import auc_score
+
+def train_test_split_custom(ratings, split_count, fraction=None):
+    """
+    Split recommendation data into train and test sets
+    
+    Params
+    ------
+    ratings : scipy.sparse matrix
+        Interactions between users and items.
+    split_count : int
+        Number of user-item-interactions per user to move
+        from training to test set.
+    fractions : float
+        Fraction of users to split off some of their
+        interactions into test set. If None, then all 
+        users are considered.
+    """
+    train = ratings.copy().tocoo()
+    test = sparse.lil_matrix(train.shape)
+    
+    if fraction:
+        try:
+            user_index = np.random.choice(
+                np.where(np.bincount(train.row) >= split_count * 2)[0], 
+                replace=False,
+                size=np.int32(np.floor(fraction * train.shape[0]))
+            ).tolist()
+        except:
+            print(('Not enough users with > {} '
+                  'interactions for fraction of {}')\
+                  .format(10, fraction))
+            raise
+    else:
+        user_index = range(train.shape[0])
+        
+    train = train.tolil()
+
+    for user in user_index:
+        test_ratings = np.random.choice(ratings.getrow(user).indices, 
+                                        size=split_count, 
+                                        replace=False)
+        train[user, test_ratings] = 0.
+        # These are just 1.0 right now
+        test[user, test_ratings] = ratings[user, test_ratings]
+   
+    
+    # Test and training are truly disjoint
+    assert(train.multiply(test).nnz == 0)
+    return train.tocsr(), test.tocsr()
+
+
+
+def sample_hyperparameters():
+    """
+    Yield possible hyperparameter choices.
+    """
+
+    while True:
+        yield {
+            "no_components": np.random.randint(16, 64),
+            "learning_schedule": np.random.choice(["adagrad", "adadelta"]),
+            "loss": "warp",
+            "learning_rate": np.random.exponential(0.05),
+            "item_alpha": np.random.exponential(1e-8),
+            "user_alpha": np.random.exponential(1e-8),
+            "max_sampled": np.random.randint(5, 15),
+            "num_epochs": np.random.randint(5, 20),
+        }
+
+
+def random_search(train, test, num_samples=10, num_threads=1):
+    """
+    Sample random hyperparameters, fit a LightFM model, and evaluate it
+    on the test set.
+
+    Parameters
+    ----------
+
+    train: np.float32 coo_matrix of shape [n_users, n_items]
+        Training data.
+    test: np.float32 coo_matrix of shape [n_users, n_items]
+        Test data.
+    num_samples: int, optional
+        Number of hyperparameter choices to evaluate.
+
+
+    Returns
+    -------
+
+    generator of (auc_score, hyperparameter dict, fitted model)
+
+    """
+
+    for hyperparams in itertools.islice(sample_hyperparameters(), num_samples):
+        num_epochs = hyperparams.pop("num_epochs")
+
+        model = LightFM(**hyperparams)
+        model.fit(train, epochs=num_epochs, num_threads=num_threads)
+
+        score = auc_score(model, test, train_interactions=train, num_threads=num_threads).mean()
+
+        hyperparams["num_epochs"] = num_epochs
+
+        yield (score, hyperparams, model)
 
 
 def generate_lightfm_recs_mapper(model, item_ids, known_items, 
